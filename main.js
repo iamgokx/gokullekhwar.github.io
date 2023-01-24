7aebd54273745c66489b8eafa456021acd92452d
Gokul Lekhwar <lekhwargokul84@gmail.com>
2023-01-24 12:11:14 +0530

    js update

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 // side nav bar toggle
-
+let hide = document.getElementById("hide")
+let sideNavBar = document.getElementById("sideNav")
 var toggleStatus = 1;
 
 document.getElementById("icon").addEventListener("click", function toggleNav() {
   if (toggleStatus == 1) {
-    document.getElementById("hide").style.height = "100vh";
-    document.getElementById("sideNav").style.width = "0";
+    hide.style.height = "100vh";
+    sideNavBar.style.width = "0";
     if (window.innerWidth > 1460) {
       moveLeft();
     }
     toggleStatus = 0;
   } else if (toggleStatus == 0) {
-    document.getElementById("hide").style.height = "0vh";
-    document.getElementById("sideNav").style.width = "20rem";
+    hide.style.height = "0vh";
+    sideNavBar.style.width = "20rem";
     if (window.innerWidth > 1460) {
       moveRight();
     }
@@ -20,34 +21,42 @@ document.getElementById("icon").addEventListener("click", function toggleNav() {
   }
 });
 
+//body shift on nav open
+
+let mainBody = document.getElementById("mainbody")
+
 function moveRight() {
-  document.getElementById("mainbody").style.marginLeft = "20rem";
+  mainBody.style.marginLeft = "20rem";
 }
 
 function moveLeft() {
-  document.getElementById("mainbody").style.marginLeft = "0rem";
+  mainBody.style.marginLeft = "0rem";
 }
 
 // top nav bar toggle
+
+let topNavBar = document.getElementById("topNav")
+let nameHeader = document.getElementById("name")
+
 var toggleStatusTop = 0;
 
 document
   .getElementById("iconTopNav")
   .addEventListener("click", function toggleTopNav() {
     if (toggleStatusTop == 1) {
-      document.getElementById("topNav").style.opacity = "0";
-      document.getElementById("topNav").style.height = "0px";
-      document.getElementById("topNav").style.width = "0px";
-      document.getElementById("name").style.animation = "none";
+      topNavBar.style.opacity = "0";
+      topNavBar.style.height = "0px";
+      topNavBar.style.width = "0px";
+      nameHeader.style.animation = "none";
       toggleStatusTop = 0;
     } else if (toggleStatusTop == 0) {
-      document.getElementById("topNav").style.height = "600px";
-      document.getElementById("topNav").style.width = "600px";
+      topNavBar.style.height = "600px";
+      topNavBar.style.width = "600px";
       setTimeout(function() {
-        document.getElementById("topNav").style.opacity = "1";
+        topNavBar.style.opacity = "1";
       }, 200);
       setTimeout(function() {
-        document.getElementById("name").style.animation = " lights 5s 750ms linear infinite";
+        nameHeader.style.animation = " lights 5s 750ms linear infinite";
       }, 200);
       
       toggleStatusTop = 1;
@@ -58,64 +67,76 @@ document
   
 // top nav bar toggle off on anchor click
 function navClick() {
-  document.getElementById("topNav").style.height = "0vh";
-  document.getElementById("topNav").style.width = "0vh";
+  topNavBar.style.height = "0vh";
+  topNavBar.style.width = "0vh";
   toggleStatusTop = 0;
 }
 
 
 //toggle theme
+let stylesheet = document.getElementById("pagestyle")
+let themeSun = document.getElementById("sun")
+let themeMoon =  document.getElementById("moon")
+
 let theme = 1;
 
 document.getElementById("themebtns").addEventListener("click", () => {
   if (theme == 1) {
-    document.getElementById("pagestyle").setAttribute("href", "lighttheme.css");
-    document.getElementById("sun").style.display = "block";
-    document.getElementById("moon").style.display = "none";
+    stylesheet.setAttribute("href", "lighttheme.css");
+    themeSun.style.display = "block";
+   themeMoon.style.display = "none";
     theme = 2;
   } else if (theme == 2) {
-    document.getElementById("pagestyle").setAttribute("href", "darktheme.css");
-    document.getElementById("moon").style.display = "block";
-    document.getElementById("sun").style.display = "none";
+    stylesheet.setAttribute("href", "darktheme.css");
+    themeMoon.style.display = "block";
+    themeSun.style.display = "none";
     theme = 1;
   }
 });
 
+
+//toggle top theme
+
+let topSun = document.getElementById("topsun")
+let topMoon = document.getElementById("topmoon")
+
 let themeTop = 1;
 document.getElementById("themebtnsTop").addEventListener("click", () => {
   if (themeTop == 1) {
-    document.getElementById("pagestyle").setAttribute("href", "lighttheme.css");
-    document.getElementById("topsun").style.display = "block";
-    document.getElementById("topmoon").style.display = "none";
+   stylesheet.setAttribute("href", "lighttheme.css");
+    topSun.style.display = "block";
+    topMoon.style.display = "none";
     themeTop = 2;
   } else if (themeTop == 2) {
-    document.getElementById("pagestyle").setAttribute("href", "darktheme.css");
-    document.getElementById("topmoon").style.display = "block";
-    document.getElementById("topsun").style.display = "none";
+   stylesheet.setAttribute("href", "darktheme.css");
+    topMoon.style.display = "block";
+    topSun.style.display = "none";
     themeTop = 1;
   }
 });
 
 //test themes side nav
+
+
 document.getElementById("darkbluetheme").addEventListener("click", () => {
-  document.getElementById("pagestyle").setAttribute("href", "styles.css");
+  stylesheet.setAttribute("href", "styles.css");
 });
 document.getElementById("orangetheme").addEventListener("click", () => {
-  document.getElementById("pagestyle").setAttribute("href", "peach.css");
+  stylesheet.setAttribute("href", "peach.css");
 });
 document.getElementById("greytheme").addEventListener("click", () => {
-  document.getElementById("pagestyle").setAttribute("href", "greytheme.css");
+  stylesheet.setAttribute("href", "greytheme.css");
 });
 
 //test themes top nav
 document.getElementById("darkbluethemetop").addEventListener("click", () => {
-  document.getElementById("pagestyle").setAttribute("href", "styles.css");
+  stylesheet.setAttribute("href", "styles.css");
 });
 document.getElementById("orangethemetop").addEventListener("click", () => {
-  document.getElementById("pagestyle").setAttribute("href", "peach.css");
+  stylesheet.setAttribute("href", "peach.css");
 });
 document.getElementById("greythemetop").addEventListener("click", () => {
-  document.getElementById("pagestyle").setAttribute("href", "greytheme.css");
+  stylesheet.setAttribute("href", "greytheme.css");
 });
 
 
